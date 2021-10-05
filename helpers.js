d658572c184dd9ae4bc4a48f8a8bd48c3fffc246
unni-settan <91621214+unni-settan@users.noreply.github.com>
2021-10-05 18:41:41 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *Amalser*:  ```" + msg + "```"
+    return "✅ *AchuMwol*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *Amalser*:  ```" + msg + "```"
+    return "🛑 *AchuMwol*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *Amalser*:  ```" + msg + "```"
+    return "⏺️ *AchuMwol*:  ```" + msg + "```"
 }
 
 
